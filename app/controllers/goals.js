d5a5ca0feb7db8dc3cf46160caea0b63f0be6ece
paulsevere <pjo2110@columbia.edu>
2016-12-16 14:34:01 -0500

    Change structure of goals model objects ot match Google LatLng.
    Updates the format and structure of the goals position data.  Changed form mongoose geojson to Google Latlng

--- app/controllers/goals.js
+++ app/controllers/goals.js
@@ -7,53 +7,48 @@ const Goal = models.goal;
 const authenticate = require('./concerns/authenticate');
 
 const index = (req, res, next) => {
-  Goal.find()
-    .then(goals => res.json({ goals }))
-    .catch(err => next(err));
+  Goal.find().then(goals => res.json({goals})).catch(err => next(err));
 };
 
 const show = (req, res, next) => {
-  Goal.findById(req.params.id)
-    .then(goal => goal ? res.json({ goal }) : next())
-    .catch(err => next(err));
+  Goal.findById(req.params.id).then(goal => goal
+    ? res.json({goal})
+    : next()).catch(err => next(err));
 };
 
 const create = (req, res, next) => {
-  let goal = Object.assign(req.body.goal, {
-    _owner: req.currentUser._id,
-  });
-  Goal.create(goal)
-    .then(goal => res.json({ goal }))
-    .catch(err => next(err));
+  let goal = req.body.goal;
+  goal = Object.assign(goal, {_owner: req.currentUser._id});
+  Goal.create(goal).then(goal => res.json({goal})).catch(err => next(err));
 };
 
 const update = (req, res, next) => {
-  let search = { _id: req.params.id, _owner: req.currentUser._id };
-  Goal.findOne(search)
-    .then(goal => {
-      if (!goal) {
-        return next();
-      }
+  let search = {
+    _id: req.params.id,
+    _owner: req.currentUser._id
+  };
+  Goal.findOne(search).then(goal => {
+    if (!goal) {
+      return next();
+    }
 
-      delete req.body._owner;  // disallow owner reassignment.
-      return goal.update(req.body.goal)
-        .then(() => res.sendStatus(200));
-    })
-    .catch(err => next(err));
+    delete req.body._owner; // disallow owner reassignment.
+    return goal.update(req.body.goal).then(() => res.sendStatus(200));
+  }).catch(err => next(err));
 };
 
 const destroy = (req, res, next) => {
-  let search = { _id: req.params.id, _owner: req.currentUser._id };
-  Goal.findOne(search)
-    .then(goal => {
-      if (!goal) {
-        return next();
-      }
+  let search = {
+    _id: req.params.id,
+    _owner: req.currentUser._id
+  };
+  Goal.findOne(search).then(goal => {
+    if (!goal) {
+      return next();
+    }
 
-      return goal.remove()
-        .then(() => res.sendStatus(200));
-    })
-    .catch(err => next(err));
+    return goal.remove().then(() => res.sendStatus(200));
+  }).catch(err => next(err));
 };
 
 module.exports = controller({
@@ -61,7 +56,12 @@ module.exports = controller({
   show,
   create,
   update,
-  destroy,
-}, { before: [
-  { method: authenticate, except: ['index', 'show'] },
-], });
+  destroy
+}, {
+  before: [
+    {
+      method: authenticate,
+      except: ['index', 'show']
+    }
+  ]
+});
--- app/models/goal.js
+++ app/models/goal.js
@@ -1,29 +1,36 @@
 'use strict';
-
+const GeoJSON = require('mongoose-geojson-schema');
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const goalSchema = new mongoose.Schema({
+const goalSchema = new Schema({
   title: {
     type: String,
-    required: true,
+    required: true
   },
-  description:{
-    type:String,
-    required:false
+  description: {
+    type: String,
+    required: false
   },
+  position: {
+    lat: Number,
+    lng: Number
+  },
+
   _owner: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Schema.Types.ObjectId,
     ref: 'User',
-    required: true,
-  },
-},{
+    required: true
+  }
+}, {
   timestamps: true,
-  toJSON: { virtuals: true },
+  toJSON: {
+    virtuals: true
+  }
 });
 
 const Goal = mongoose.model('Goal', goalSchema);
 
 module.exports = Goal;
-
 
 // title, description, user_id,
