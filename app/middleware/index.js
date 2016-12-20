2276fd140fdc004dab2fbb56da60d40778cca94b
paulsevere <pjo2110@columbia.edu>
2016-12-20 11:17:19 -0500

    Reset cors settings
    Reset cors setting so I can follow directions properly

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -12,7 +12,7 @@ const loader = require('lib/wiring/loader');
 const corsPort = +('GA'.split('').reduce((p, c) => p + c.charCodeAt(), ''));
 
 const corsOptions = {
-  origin: `https://bearpigman.github.io/` || process.env.CLIENT_ORIGIN
+  origin: process.env.CLIENT_ORIGIN || `http://localhost:${corsPort}`
 };
 
 const before = (app) => {
